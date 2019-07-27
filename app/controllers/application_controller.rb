915d2165af76b2ab2eae0029fff53bc379fc7a74
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-26 20:23:10 -0600

    Adds sessions controller new action and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :current_user?, :date
+
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin!
+    if current_user.nil?
+      four_oh_four
+    else
+      four_oh_four unless current_user.admin?
+    end
+  end
+
+  def require_user!
+    if current_user.nil?
+      four_oh_four
+    else
+      four_oh_four unless current_user.user?
+    end
+  end
 end
