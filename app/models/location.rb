ced00cd2f4c65048d0214cde7380675ed3225964
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-26 19:48:11 -0600

    Adds relationships to models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
   validates_presense_of :name
+
+  has_many :machines
 end
--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,5 @@
 class Machine < ApplicationRecord
   validates_presense_of :name, :status, :type
+
+  belongs_to :location
 end
