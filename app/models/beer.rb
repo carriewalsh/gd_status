dfe548e9745e4eeab2d1314d2b56f472d4422c81
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-27 17:01:32 -0600

    Adds category to beer for radio display

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,5 @@
 class Beer < ApplicationRecord
   validates_presence_of :name, :icon
+
+  enum category: ["year_round", "seasonal", "tank_farm", "yeti", "wood_werks"]
 end
---
+++ db/migrate/20190727225746_add_categories_to_beers.rb
@@ -0,0 +1,5 @@
+class AddCategoriesToBeers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :beers, :category, :integer
+  end
+end
