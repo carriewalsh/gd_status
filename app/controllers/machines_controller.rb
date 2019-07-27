dd5c85835c0ca238b4e0be2fada2b64b2283b2aa
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-27 16:57:32 -0600

    Adds local vars for beer types

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -3,6 +3,11 @@ class MachinesController < ApplicationController
 
   def index
     @locations = Location.all
+    @year_round = Beer.where(category: 'year_round')
+    @seasonal = Beer.where(category: 'seasonal')
+    @tank_farm = Beer.where(category: 'tank_farm')
+    @yeti = Beer.where(category: 'yeti')
+    @wood_werks = Beer.where(category: 'wood_werks')
   end
 
   def update
